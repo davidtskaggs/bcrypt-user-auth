c1da9994be14eb9f33a508b108ca78c371fbaa72
David Skaggs <davidtskaggs@gmail.com>
2017-10-03 10:24:19 -0400

    Update files

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,15 +1,12 @@
 get '/users/new' do
-  # why do we need to create a new user here?
-  # hint: look at the users/new view
   @user = User.new
   erb :'users/new'
 end
 
-# what happens if this route is placed above the users/new route?
 get '/users/:id' do
   @user = User.find_by(id: params[:id])
 
-  # this line protects the unauthorized showing of user data
+
   redirect '/' unless @user.id == session[:user_id]
 
   erb :'users/show'
@@ -21,11 +18,6 @@ post '/users' do
     session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
   else
-    # on save fail, rerendering the users/new view allows us
-    # to show the user verbose error messages
-    # so they know why user create failed.
-    # take a look at the users/new view to see
-    # how the error messages are displayed
     erb :'users/new'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,8 @@ class User < ApplicationRecord
     self.password_hash = @password
   end
 
-  # e.g., User.authenticate('jesse@devbootcamp.com', 'apples123')
+
   def self.authenticate(email, plaintext_password)
-    # if email and password correspond to a valid user, return that user
-    # otherwise, return nil
     return nil unless user = find_by(email: email)
     return user if user.password == plaintext_password
     return nil
