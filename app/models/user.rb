fe91e8076654b762dd5d776dfd1285db10c59947
David Skaggs <davidtskaggs@gmail.com>
2017-10-24 11:56:14 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,24 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, :password, presence: true
+  validates :first_name, :last_name, :email, presence: true
+  validates :password, length: {minimum: 6}
   validates :email, uniqueness: true
+  
+  has_secure_password
+#   has_secure_password replaces all of the below methods
+  
+#   def password
+#     @password ||= BCrypt::Password.new(password_hash)
+#   end
 
-  def password
-    @password ||= BCrypt::Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = BCrypt::Password.create(new_password)
-    self.password_hash = @password
-  end
+#   def password=(new_password)
+#     @password = BCrypt::Password.create(new_password)
+#     self.password_hash = @password
+#   end
 
 
-  def self.authenticate(email, plaintext_password)
-    return nil unless user = find_by(email: email)
-    return user if user.password == plaintext_password
-    return nil
-  end
+#   def self.authenticate(email, plaintext_password)
+#     return nil unless user = find_by(email: email)
+#     return user if user.password == plaintext_password
+#     return nil
+#   end
 end
